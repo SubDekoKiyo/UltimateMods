9a63326939fed70fbff3ddd071c1ba1def8a47ef
Dekokiyo <dekobokokiyomori0817@gmail.com>
2022-12-22 17:42:52 +0900

    更新

--- UltimateMods/Roles/RoleInfo/RoleInfoData.cs
+++ UltimateMods/Roles/RoleInfo/RoleInfoData.cs
@@ -5,14 +5,12 @@ public class RoleInfo
     public Color RoleColor;
     public virtual string Name { get { return ModTranslation.getString(NameKey); } }
     public virtual string ColorName { get { return Helpers.cs(RoleColor, Name); } }
-    public virtual string IntroDescription { get { return ModTranslation.getString(NameKey + "IntroDesc"); } }
-    public virtual string ShortDescription { get { return ModTranslation.getString(NameKey + "ShortDesc"); } }
-    public virtual string FullDescription { get { return ModTranslation.getString(NameKey + "FullDesc"); } }
+    public virtual string IntroDescription { get { return ModTranslation.getString(NameKey + "Intro"); } }
+    public virtual string ShortDescription { get { return ModTranslation.getString(NameKey + "Short"); } }
+    public virtual string FullDescription { get { return ModTranslation.getString(NameKey + "Full"); } }
     public virtual string RoleOptions { get { return GameOptionsDataPatch.optionsToString(BaseOption, true); } }
 
-    public bool Enabled { get { return CustomOptionsH.ActivateModRoles.getBool() && (BaseOption == null || BaseOption.enabled); } }
     public RoleId RoleId;
-
     public string NameKey;
     public CustomOption BaseOption;
 
